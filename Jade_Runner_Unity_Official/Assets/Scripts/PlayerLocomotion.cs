8ece51e6f801f9fb70a4e12ee4142413bf1d04ef
Nick <ncazmay@student.fitchburgstate.edu>
2019-10-16 15:22:27 -0400

    Model + Animation Machine Started. Controller Input added
    Stick input is a bit chunky; needs tuning

--- Jade_Runner_Unity_Official/Assets/Scripts/PlayerLocomotion.cs
+++ Jade_Runner_Unity_Official/Assets/Scripts/PlayerLocomotion.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
+    public float stickDeadZone = 0.25f;
     public float moveSpeed;
     public float lookSpeed = 5f;
     private float turnAngle;
     private Quaternion targetRotation;
 
     private Vector2 playerInput;
+
+    private string jumpControl;
+
+    private string jumpKeyboard = "Jump";
+    private string jumpJoystick = "Jump2";
 
     [SerializeField]
     private int fruitCount;
@@ -18,8 +24,10 @@ public class PlayerLocomotion : MonoBehaviour
     public bool dblJump = false;
     public bool inKillbox = false;
     public bool canJump = true;
+    public bool keyboardActive;
+    public bool controllerActive;
     [SerializeField]
-    private float killTimer = 10.0f;
+    private float killTimer = 3.0f;
     [SerializeField]
     private float jumpTimer;
     [SerializeField]
@@ -64,6 +72,15 @@ public class PlayerLocomotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(keyboardActive && !controllerActive)
+        {
+            jumpControl = jumpKeyboard;
+        }
+        else if(controllerActive && !keyboardActive)
+        {
+            jumpControl = jumpJoystick;
+        }
+
         jumpTimer -= 0.1f;
         if(inKillbox)
         {
@@ -71,12 +88,12 @@ public class PlayerLocomotion : MonoBehaviour
         }
         else
         {
-            killTimer = 10.0f;
+            killTimer = 3.0f;
         }
         if(killTimer < 0)
         {
             health -= 1;
-            killTimer = 10.0f;
+            killTimer = 3.0f;
         }
         //NEED CONDITION + CODE FOR DEATH, FADE TO BLACK, AND RESPAWN. GO BACK AND REVIEW PRISONER CODE
 
@@ -85,7 +102,7 @@ public class PlayerLocomotion : MonoBehaviour
             dblJumpTimer -= 0.1f;
         }
        
-        if(Input.GetButtonUp("Jump") && airBorne && jumpTimer <= 2.5f && jumpCount < 1)
+        if(Input.GetButtonUp(jumpControl) && airBorne && jumpTimer <= 2.5f && jumpCount < 1)
         {
             dblJump = true;
             jumpCount += 1;
@@ -94,7 +111,7 @@ public class PlayerLocomotion : MonoBehaviour
         //if(!noInput) <- will be used to decide if player is moving or not for idle animation
         //{ }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown(jumpControl))
         {
             if (!airBorne && jumpTimer <= 0)
             {
@@ -104,14 +121,14 @@ public class PlayerLocomotion : MonoBehaviour
                 canJump = false;
             }
         }
-        if(Input.GetButtonDown("Jump") && dblJumpTimer < 0)
+        if(Input.GetButtonDown(jumpControl) && dblJumpTimer < 0)
         {
             rb.AddForce(Vector3.up * dblJumpForce, ForceMode.Impulse);
             dblJump = false;
             dblJumpTimer = 0;
         }
 
-        if(Input.GetButtonUp("Jump"))
+        if(Input.GetButtonUp(jumpControl))
         {
             canJump = true;
         }
@@ -130,12 +147,32 @@ public class PlayerLocomotion : MonoBehaviour
         CalcDirect();
         PlayerRotate();
         PlayerMove();
+        //PlayerMoveKeyboard()
+        //PlayerMoveController()
     }
 
     void PlayerInput()
     {
-        playerInput.x = Input.GetAxisRaw("Horizontal");
-        playerInput.y = Input.GetAxisRaw("Vertical");
+        if(keyboardActive)
+        {
+            playerInput.x = Input.GetAxisRaw("Horizontal");
+            playerInput.y = Input.GetAxisRaw("Vertical");
+        }
+        else if(controllerActive)
+        {
+            //controlstick input here
+            playerInput.x = Input.GetAxisRaw("Horizontal2");
+            playerInput.y = Input.GetAxisRaw("Vertical2");
+
+            if(playerInput.magnitude < stickDeadZone)
+            {
+                playerInput = Vector2.zero;
+            }
+            else
+            {
+                playerInput = playerInput.normalized * ((playerInput.magnitude - stickDeadZone) / (1 - stickDeadZone));
+            }
+        }
     }
 
     void CalcDirect()

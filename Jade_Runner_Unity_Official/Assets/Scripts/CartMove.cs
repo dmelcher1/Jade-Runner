27773cf86a879cd982fa4de500fe1eca9fa89da1
ZacharyLapidus <44439798+ZacharyLapidus@users.noreply.github.com>
2019-11-27 18:29:44 -0500

    Added cart move code

---
+++ Jade_Runner_Unity_Official/Assets/Scripts/CartMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMove : MonoBehaviour
+{
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        {
+            Debug.Log("Move");
+            transform.Translate(0f, 3f, 0f);
+        }
+    }
+}

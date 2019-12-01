977fd41b96a8d7bb633fb8c8ff14ca55f548a53f
Ethan Reese <ereese@berklee.edu>
2019-12-01 12:44:10 -0500

    Adding in footstep sfx and such

---
+++ Jade_Runner_Unity_Official/Assets/Scripts/AudioScripts/FootSteps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    uint bankID;
+
+    public void LoadBank()
+    {
+        AkSoundEngine.LoadBank("JadeRunner_Soundbank", AkSoundEngine.AK_DEFAULT_POOL_ID, out bankID);
+    }
+    // Update is called once per frame
+    private void Step()
+    {
+        AkSoundEngine.PostEvent("UI_hover", gameObject);
+
+    }
+}

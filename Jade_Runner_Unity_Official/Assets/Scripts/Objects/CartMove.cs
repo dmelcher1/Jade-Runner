f446379b5f893d6dc2808b73b1f552f1d072495b
ZacharyLapidus <44439798+ZacharyLapidus@users.noreply.github.com>
2019-12-03 16:33:22 -0500

    Starting on the finishing touches for the level
    Added bamboo parks
    added more bamboo fences
    Added in cart blockers

--- Jade_Runner_Unity_Official/Assets/Scripts/Objects/CartMove.cs
+++ Jade_Runner_Unity_Official/Assets/Scripts/Objects/CartMove.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CartMove : MonoBehaviour
 {
     private Vector3 startPos;
+    Collider m_collider;
+    public float moveUp;
     // Start is called before the first frame update
     void Start()
     {
         startPos = this.transform.position;
+        m_collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -18,9 +21,9 @@ public class CartMove : MonoBehaviour
     }
     void OnTriggerExit(Collider other)
     {
-        {
-            Debug.Log("Move");
-            transform.Translate(0f, 3f, 0f);
-        }
+        
+            Debug.Log("Move and Collider Off");
+            transform.Translate(0f, moveUp, 0f);
+            m_collider.enabled = !m_collider.enabled;
     }
 }
